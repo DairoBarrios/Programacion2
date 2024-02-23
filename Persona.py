e56304f8238a1e4cf9a675708b20b97595d59675
Dairo Barrios <dairobarriosrivero@gmail.com>
2024-02-23 09:01:38 -0500

    Subida de actividad 1

---
+++ Persona.py
@@ -0,0 +1,35 @@
+class Persona:
+    def __init__(self,nombre,apellido,correo,telefono,cedula):
+        
+        self.nombre=nombre
+        self.apellido=apellido
+        self.correo=correo
+        self.telefono=telefono
+        self.cedula=cedula
+        
+        
+    def Obtenernombre(self):
+        return f'mi nombre es {self.nombre}'
+    
+    def Obtenerapellido(self):
+        return f'mi apellido es {self.apellido}'
+    
+    def Obtenercorreo(self):
+        return f'mi correo es {self.correo}'
+    
+    def Obtenertelefono(self):
+        return f'mi telefono es {self.telefono}'
+    
+    def Obtenercedula(self):
+        return f'mi cedula es {self.cedula}'
+    
+           
+print("Nombre","Dairo","apellido","Barrios","correo","Dairobarriosrivero@gmail.com","telefono","3127355968","cedula","1142914875")
+         
+ 
+
+           
+                       
+        
+    
+                
